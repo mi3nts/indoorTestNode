e139abbdc35ef26767fddde6d72643c93a12b403
lakihaomal <lhw150030@utdallas.edu>
2023-03-08 10:33:40 -0600

    sensor calibration updates

--- firmware/xu4Mqtt/mintsXU4/mintsProcessing.py
+++ firmware/xu4Mqtt/mintsXU4/mintsProcessing.py
@@ -271,7 +271,7 @@ def climateDataPrepV2(nodeID,climateSensor,WIMDA,YXXDR,mergedPklsFolder):
     print("Reading Data for Node: " + nodeID)
     climateSensor   = superReader(nodeID,climateSensor)
     mintsData = merger([climateSensor, WIMDA,YXXDR])
-    pd.to_pickle(mintsData,getPathGeneric(mergedPklsFolder,nodeID,str(climateSensor),"pkl") )
+    pd.to_pickle(mintsData,getPathGeneric(mergedPklsFolder,nodeID,"climateData","pkl") )
     print(mintsData)
 
     
