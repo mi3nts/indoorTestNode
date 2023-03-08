b37fc46bcc6dc98944eff111510e89b48fa2b354
lakihaomal <lhw150030@utdallas.edu>
2023-03-08 10:16:56 -0600

    sensor calibration updates

--- firmware/xu4Mqtt/c_2_dataPrep.py
+++ firmware/xu4Mqtt/c_2_dataPrep.py
@@ -24,22 +24,22 @@ mergedPklsFolder     = mD.mergedPklsFolder
 # BME280  = mP.superReader("001e06323a06","BME280");
 # print(BME280)
 
-# YXXDR     = mP.superReader(airMarID,"YXXDR")
-# pd.to_pickle(YXXDR ,mP.getPathGeneric(referencePklsFolder,airMarID,"YXXDR","pkl") )
+YXXDR     = mP.superReader(airMarID,"YXXDR")
+pd.to_pickle(YXXDR ,mP.getPathGeneric(referencePklsFolder,airMarID,"YXXDR","pkl") )
 
 WIMDA     = mP.superReader(airMarID,"WIMDA")
 pd.to_pickle(WIMDA ,mP.getPathGeneric(referencePklsFolder,airMarID,"WIMDA","pkl") )
 
-# WIMDA  = pd.read_pickle(mP.getPathGeneric(referencePklsFolder,airMarID,"WIMDA","pkl"))
-# YXXDR  = pd.read_pickle(mP.getPathGeneric(referencePklsFolder,airMarID,"YXXDR","pkl"))
+WIMDA  = pd.read_pickle(mP.getPathGeneric(referencePklsFolder,airMarID,"WIMDA","pkl"))
+YXXDR  = pd.read_pickle(mP.getPathGeneric(referencePklsFolder,airMarID,"YXXDR","pkl"))
 
-# print("--------------")
+print("--------------")
 
-# climateSensors = ["BME280","BME680","SCD30"]
+climateSensors = ["BME280","BME680","SCD30"]
 
-# for climateSensor in climateSensors:
-#     print("=====================MINTS=====================")
-#     print("Prepearing Climate data for Node: " + nodeID +" with Climate Sensor: " + climateSensor)
-#     print("-----------------------------------------------")
-#     mP.climateDataPrepV2(nodeID,climateSensor,WIMDA,YXXDR,mergedPklsFolder)
+for climateSensor in climateSensors:
+    print("=====================MINTS=====================")
+    print("Prepearing Climate data for Node: " + nodeID +" with Climate Sensor: " + climateSensor)
+    print("-----------------------------------------------")
+    mP.climateDataPrepV2(nodeID,climateSensor,WIMDA,YXXDR,mergedPklsFolder)
 
