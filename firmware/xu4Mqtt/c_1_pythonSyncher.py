8d5b7a66ef5deb20433f4a11bb659e76b361394d
lakihaomal <lhw150030@utdallas.edu>
2023-03-07 18:08:37 -0600

    air mar calibration parametors added

--- firmware/xu4Mqtt/c_1_pythonSyncher.py
+++ firmware/xu4Mqtt/c_1_pythonSyncher.py
@@ -17,15 +17,15 @@ nodeID         = mD.nodeID
 dataFolder     = mD.dataFolder
 
 airMarID       = mD.airMarID
-dataFolderRef  = dataFolder+ "/reference/"
-dataFolderRaw  = dataFolder+ "/raw/"
+dataFolderRef  = mD.dataFolderReference
+dataFolderRaw  = mD.dataFolder
 
 
 print("Data Folder: " + dataFolder)
 print("Data Folder Raw: " + dataFolderRaw)
 print("Data Folder Ref: " + dataFolderRef)
 
-sysStr = 'rsync -avzrtu -e "ssh" ' +  "--include='2023_03*' --include='*.csv' --include='*/' --exclude='*' mints@10.247.238.16:Downloads/reference/" + airMarID + " " + dataFolderRef
+sysStr = 'rsync -avzrtu -e "ssh" ' +  "--include='2023_03_*' --include='*.csv' --include='*/' --exclude='*' mints@10.247.238.16:Downloads/reference/" + airMarID + " " + dataFolderRef
 print(sysStr)
 # os.system(sysStr)
 
