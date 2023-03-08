780ad75532292991e9e1e3c049ceec3d44f64dc0
lakihaomal <lhw150030@utdallas.edu>
2023-03-08 10:01:58 -0600

    sensor calibration updates

--- firmware/xu4Mqtt/c_2_dataPrep.py
+++ firmware/xu4Mqtt/c_2_dataPrep.py
@@ -24,11 +24,11 @@ mergedPklsFolder     = mD.mergedPklsFolder
 # BME280  = mP.superReader("001e06323a06","BME280");
 # print(BME280)
 
-#YXXDR     = mP.superReader(airMarID,"YXXDR")
-#pd.to_pickle(YXXDR ,mP.getPathGeneric(referencePklsFolder,airMarID,"YXXDR","pkl") )
+YXXDR     = mP.superReader(airMarID,"YXXDR")
+pd.to_pickle(YXXDR ,mP.getPathGeneric(referencePklsFolder,airMarID,"YXXDR","pkl") )
 
-#WIMDA     = mP.superReader(airMarID,"WIMDA")
-#pd.to_pickle(WIMDA ,mP.getPathGeneric(referencePklsFolder,airMarID,"WIMDA","pkl") )
+WIMDA     = mP.superReader(airMarID,"WIMDA")
+pd.to_pickle(WIMDA ,mP.getPathGeneric(referencePklsFolder,airMarID,"WIMDA","pkl") )
 
 WIMDA  = pd.read_pickle(mP.getPathGeneric(referencePklsFolder,airMarID,"WIMDA","pkl"))
 YXXDR  = pd.read_pickle(mP.getPathGeneric(referencePklsFolder,airMarID,"YXXDR","pkl"))
